cd0aaf7dc19c6b1baded19834f19e7a08e652c08
Azuko <tazumaki@mail.ru>
2023-05-29 00:17:57 +0300

    Шаблоны и их стилизация (v1.0.0)

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector("body").classList.add("loaded");
+    }, 500)
+  });
+
+(function ($) {
+    $(window).on("load", function () {
+        $('body').mCustomScrollbar({
+            theme: "inset-3-dark",
+            autoHideScrollbar: true,
+            mouseWheel: {
+                deltaFactor: 8,
+                normalizeDelta: false
+            },
+            scrollButtons: {
+                enable: true
+            }
+        });
+    });
+})(jQuery);
