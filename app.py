5e97d81b7d596f49c302e1d83656b04ee1ca79e9
Azuko <tazumaki@mail.ru>
2023-05-29 18:49:27 +0300

    Шаблоны и их стилизация (v1.2.0)

--- app.py
+++ app.py
@@ -8,5 +8,29 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/basketball_players', methods=['POST', 'GET'])
+def basketball_players():
+    new_player = request.form
+    return render_template('answers/basketball_players.html')
+
+
+@app.route('/translate', methods=['POST', 'GET'])
+def translate():
+    new_word = request.form
+    return render_template('answers/translate.html')
+
+
+@app.route('/company_employees', methods=['POST', 'GET'])
+def company_employees():
+    new_employee = request.form
+    return render_template('answers/company_employees.html')
+
+
+@app.route('/books', methods=['POST', 'GET'])
+def books():
+    new_book = request.form
+    return render_template('answers/books.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
