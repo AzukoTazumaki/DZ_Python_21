a142a739371c3cc3ed5eec40f4b93cd6d33a64cb
Azuko <tazumaki@mail.ru>
2023-05-29 17:00:36 +0300

    Шаблоны и их стилизация (v1.1.0)

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,11 @@
+/* PRELOADER */
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         document.querySelector("body").classList.add("loaded");
     }, 500)
   });
 
+/* SCROLLBAR */
 (function ($) {
     $(window).on("load", function () {
         $('body').mCustomScrollbar({
@@ -19,3 +21,9 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 })(jQuery);
+
+/* CUSTOM INPUT MASK */
+const InputMask = require('inputmask')
+const phone_input = document.getElementById('employee_phone_number')
+let phone = new InputMask('+7(928)-555-55-55')
+phone.mask(phone_input)
