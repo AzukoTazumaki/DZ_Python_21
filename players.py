4c4e0813fde51005097ae11a3ea58a7f351b859c
Azuko <tazumaki@mail.ru>
2023-06-04 16:27:19 +0300

    Баскетболисты (Задание 1) (v.1.1.0)
    - добавлены аннотации

--- players.py
+++ players.py
@@ -22,15 +22,15 @@ class Players:
 
     @staticmethod
     def parse_site():
-        url = 'https://www.espn.com'
+        url: str = 'https://www.espn.com'
         response = requests.get(url + '/nba/teams')
         response_body = bs(response.content, 'html.parser')
-        needed_hrefs = [
+        needed_hrefs: list = [
             link["href"] for link in response_body
             .find('div', {'class': 'layout is-split'})
             .find_all('a', {'class': 'AnchorLink'}) if 'roster' in link["href"]
         ]
-        columns = ['Name', 'Position', 'Age', 'Height', 'Weight', 'Team', 'College', 'Salary']
+        columns: list = ['Name', 'Position', 'Age', 'Height', 'Weight', 'Team', 'College', 'Salary']
         df_w = pd.DataFrame([columns], columns=columns)
         df_w.to_csv('results/players.csv', mode='w', index=False, header=False)
         for href in needed_hrefs:
@@ -38,13 +38,13 @@ class Players:
                 .find('tbody', {'class': 'Table__TBODY'})\
                 .find_all('tr', {'class': 'Table__TR'})
             for table_row in table_rows:
-                table_datas = table_row.find_all('td', {'class': 'Table__TD'})
-                player_name = ' '.join(re.findall('[A-z.]+', table_datas[1].text))
-                player_role = table_datas[2].text
-                player_age = table_datas[3].text
-                player_height = round(Length(float('.'.join(re.findall(r'[1-9]+', table_datas[4].text))), LengthUnits.Foot).centimeters)
-                player_weight = round(Mass(int(re.findall(r'[0-9]+', table_datas[5].text)[0]), MassUnits.Pound).kilograms)
-                player_team = ' '.join(
+                table_datas: list = table_row.find_all('td', {'class': 'Table__TD'})
+                player_name: str = ' '.join(re.findall('[A-z.]+', table_datas[1].text))
+                player_role: str = table_datas[2].text
+                player_age: str = table_datas[3].text
+                player_height: int = round(Length(float('.'.join(re.findall(r'[1-9]+', table_datas[4].text))), LengthUnits.Foot).centimeters)
+                player_weight: int = round(Mass(int(re.findall(r'[0-9]+', table_datas[5].text)[0]), MassUnits.Pound).kilograms)
+                player_team: str = ' '.join(
                     [
                         team.text for team in bs(requests.get(url + href).content, 'html.parser')
                         .find('main', {'id': 'fittPageContainer'})
@@ -52,14 +52,14 @@ class Players:
                         .find_all('span', {'class': 'db'})
                     ]
                 )
-                player_info_unknown = 'Неизвестно'
-                player_education = player_info_unknown if re.search(r'-', table_datas[6].text) else table_datas[6].text
-                player_salary_usd = ''.join(''.join(re.findall(r'[0-9,]+', table_datas[7].text)).split(','))
-                player_salary = player_info_unknown \
+                player_info_unknown: str = 'Неизвестно'
+                player_education: str = player_info_unknown if re.search(r'-', table_datas[6].text) else table_datas[6].text
+                player_salary_usd: str = ''.join(''.join(re.findall(r'[0-9,]+', table_datas[7].text)).split(','))
+                player_salary: str = player_info_unknown \
                     if re.search(r'-', table_datas[7].text) \
                     else round(cc(fallback_on_wrong_date=True).convert(int(player_salary_usd), 'USD', 'RUB') / 1000000)
 
-                player_info_list = [
+                player_info_list: list = [
                     player_name, player_role, player_age, player_height,
                     player_weight, player_team, player_education, player_salary
                 ]
@@ -67,8 +67,8 @@ class Players:
                 df_a.to_csv('results/players.csv', mode='a', index=False, header=False)
 
     def add_player_to_csv(self):
-        columns = ['Name', 'Position', 'Age', 'Height', 'Weight', 'Team', 'College', 'Salary']
-        new_player_info = [
+        columns: list = ['Name', 'Position', 'Age', 'Height', 'Weight', 'Team', 'College', 'Salary']
+        new_player_info: list = [
             self.full_name, self.position, self.age, self.height,
             self.weight, self.team, self.education, self.salary
         ]
@@ -80,7 +80,7 @@ class Players:
     @staticmethod
     def check_unknown_fields(player_info: list):
         for index, field in enumerate(player_info):
-            if field == '' or field == '  ':
+            if field == '' or len(field) == 2:
                 player_info[index] = 'Неизвестно'
             continue
         return player_info
